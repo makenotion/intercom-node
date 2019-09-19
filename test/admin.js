e02cac0391587b8fe7e14f08c613e36c1d201c2c
Jamie Quint <jamiequint@gmail.com>
2019-09-19 14:06:56 -0700

    fix test formatting

--- test/admin.js
+++ test/admin.js
@@ -28,11 +28,11 @@ describe('admins', () => {
     });
   });
   it('should update admin away mode and reassign settings', done => {
-    nock('https://api.intercom.io').put('/admins/baz/away').reply(200, { away_mode_enabled: true, away_mode_reassign: false }).reply(200, {});
+    nock('https://api.intercom.io').put('/admins/baz/away', { away_mode_enabled: true, away_mode_reassign: false }).reply(200, {});
     const client = new Client('foo', 'bar').usePromises();
     client.admins.away('baz', { away_mode_enabled: true, away_mode_reassign: false }).then(r => {
-        assert.equal(200, r.statusCode);
-        done();
-      });
+      assert.equal(200, r.statusCode);
+      done();
+    });
   });
 });
